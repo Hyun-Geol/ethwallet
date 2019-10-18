0a5a72d55059fd68a9184ea7ee356fab29a6ca9c
whgusrjf33 <whgusrjf33@naver.com>
2019-10-18 17:19:36 +0900

    PW, PrivateKey 암호화 및 자잘한 오류 수정

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res){
+       let title = 'Login';
+    return res.render(`index`, { title });
 });
 
-module.exports = router;
+router.get('/fail', function (req, res) {
+    let title = 'Error'
+    return res.render('fail', { title })
+})
+
+router.get('/overlap', function (req, res) {
+    let title = 'Overlap'
+    return res.render('overlap', { title })
+})
+
+router.get('/permission', function (req, res) {
+    let title = 'Permission'
+    return res.render('permission', { title })
+})
+
+module.exports = router;    
