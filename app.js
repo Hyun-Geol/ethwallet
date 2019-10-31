d3a3a43b4b5149285dd188112e2c052f0f0ba1f2
whgusrjf33 <whgusrjf33@naver.com>
2019-10-31 09:45:41 +0900

    HTML logger(morgan)추가

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var topicRouter = require('./routes/topic');
@@ -7,6 +8,7 @@ var topicRouter = require('./routes/topic');
 app.use('/public', express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
 
+app.use(logger('dev'));
 app.use('/', indexRouter);
 app.use('/topic', topicRouter);
 
